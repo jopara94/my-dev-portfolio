32ae3686ffc86899830669c9c511645181f3fb6e
James Opara <jopara94@gmail.com>
2020-11-23 16:13:01 -0600

    test

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -42,6 +42,16 @@ class Resume extends Component {
 
     return (
       <section id="resume">
+        <div className="row work">
+          <div className="three columns header-col">
+            <h1>
+              <span>Work</span>
+            </h1>
+          </div>
+
+          <div className="nine columns main-col">{work}</div>
+        </div>
+        
         <div className="row education">
           <div className="three columns header-col">
             <h1>
